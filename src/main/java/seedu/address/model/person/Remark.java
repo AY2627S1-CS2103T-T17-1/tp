36bc13b51e9d932524d5cd1b440cd0655b18e07e
Lim Qi Zao <brianlimqz@gmail.com>
2026-09-17 19:17:10 +0800

    Tutorial adding remark

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,36 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a Person's remark in the address book.
+ * Guarantees: immutable; is always valid.
+ */
+public class Remark {
+    public final String value;
+
+    /**
+     * Creates a {@code Remark} containing the given text.
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof Remark otherRemark
+                && value.equals(otherRemark.value));
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
